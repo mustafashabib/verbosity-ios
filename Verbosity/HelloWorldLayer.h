db88de62674b970d67b205e6e7f57093d2a1f993
Mustafa Shabib <mustafashabib@gmail.com>
2012-06-20 19:07:48 -0500

    Initial Commit

---
+++ Verbosity/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  Verbosity
+//
+//  Created by Mustafa Shabib on 6/20/12.
+//  Copyright We Are Mammoth 2012. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Verbosity/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Verbosity
+//
+//  Created by Mustafa Shabib on 6/20/12.
+//  Copyright We Are Mammoth 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
